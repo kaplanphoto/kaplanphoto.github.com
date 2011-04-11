883f1a15d479569953961b3fc5cbd0edfa03f9da
joehand <joe@joehand.org>
2011-04-10 19:28:50 -0700

    commit some changes before big overhall

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@
       'dress' : [
         { shape: 'square', resolution: 14 },
         { shape: 'circle', resolution: 14, offset: 10, alpha: 0.5 },
-        { shape: 'diamond', resolution: 4, offset: 22 },
-        { shape: 'circle', resolution: 8, size: 9, offset: 10, alpha: 0.5 }
+        { shape: 'diamond', resolution: 24, offset: 22 },
+        { shape: 'circle', resolution: 8, size: 14, offset: 25, alpha: 0.3 }
       ]
     };
 
